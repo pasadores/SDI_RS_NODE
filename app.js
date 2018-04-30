a4247143189ec5f369d92080f4f2463ef88f7b86
danigg <donigg93@gmail.com>
2018-04-30 20:06:59 +0200

    Empezando...

--- app.js
+++ app.js
@@ -1,41 +1,39 @@
-var createError = require('http-errors');
+// Módulos
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var expressSession = require('express-session');
+app.use(expressSession({
+    secret: 'abcdefg',
+    resave: true,
+    saveUninitialized: true
+}));
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+var crypto = require('crypto');
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+var mongo = require('mongodb');
+var swig = require('swig');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+var gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
+
+app.use(express.static('public'));
+
+app.set('port', 8081);
+app.set('db','mongodb://admin:sdi@ds131989.mlab.com:31989/tiendamusica');
+app.set('clave','abcdefg');
+app.set('crypto',crypto);
+
+require("./routes/rusuarios.js")(app,swig, gestorBD);
+
+app.get('/', function (req, res) {
+    res.redirect('/identificarse');
+})
+
+// lanzar el servidor
+app.listen(app.get('port'), function() {
+    console.log("Servidor activo");
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
