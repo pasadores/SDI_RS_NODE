93d3ec96b0f3ad1f42b0b0ec91e5fe467ac26c49
Pablo Amorin Triana <pabloarrobaconb@gmail.com>
2018-05-01 17:44:30 +0200

    Terminados casos de uso 1 y 2

--- routes/rusuarios.js
+++ routes/rusuarios.js
@@ -6,20 +6,41 @@ module.exports = function(app, swig, gestorBD) {
     });
 
     app.post('/usuario', function(req, res) {
-        var seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
-            .update(req.body.password).digest('hex');
-        var usuario = {
-            email : req.body.email,
-            password : seguro
+
+        if(req.body.password != req.body.password2){
+            res.redirect("/registrarse?mensaje=Las contraseñas no coinciden")
+        }
+        else{
+            var seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
+                .update(req.body.password).digest('hex');
+            var criterio = {
+                email : req.body.email,
+                password : seguro
+            }
+            gestorBD.obtenerUsuarios(criterio, function(usuarios) {
+                if (usuarios == null || usuarios.length == 0) {
+                    var usuario = {
+                        email : req.body.email,
+                        nombre : req.body.nombre,
+                        password : seguro
+                    }
+
+                    gestorBD.insertarUsuario(usuario, function(id) {
+                        if (id == null){
+                            res.redirect("/registrarse?mensaje=Error al registrar usuario")
+                        } else {
+                            res.redirect("/identificarse?mensaje=Nuevo usuario registrado");
+                        }
+                    });
+                } else {
+                    res.redirect("/registrarse?mensaje=El email ya existe");
+                }
+            });
+
+
         }
 
-        gestorBD.insertarUsuario(usuario, function(id) {
-            if (id == null){
-                res.send("Error al insertar ");
-            } else {
-                res.send('Usuario Insertado ' + id);
-            }
-        });
+
     });
 
     app.get("/identificarse", function(req, res) {
@@ -37,7 +58,9 @@ module.exports = function(app, swig, gestorBD) {
         gestorBD.obtenerUsuarios(criterio, function(usuarios) {
             if (usuarios == null || usuarios.length == 0) {
                 req.session.usuario = null;
-                res.send("No identificado: ");
+                res.redirect("/identificarse" +
+                    "?mensaje=Email o password incorrecto"+
+                    "&tipoMensaje=alert-danger ");
             } else {
                 req.session.usuario = usuarios[0].email;
                 res.send("identificado");
